24ab64048575628f8b56abd8c6347cb31cb38ae8
Vivien <samanthaservo09@gmail.com>
2024-05-05 00:55:30 +0800

    added required packages

--- existing_svm_algorithm/existing_svm.py
+++ existing_svm_algorithm/existing_svm.py
@@ -8,5 +8,18 @@
 7. SVM assigns class labels based on their position as a training result. (Problem 2)
 8. Performance will be evaluated with the common four indicators of accuracy rate, precision rate, recall rate and F1 value.
 """
+#necessary libraries
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.metrics import accuracy_score
 
 #data preprocessing
