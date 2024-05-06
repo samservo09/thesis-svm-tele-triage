c3ce6dca9f9c48ec04087244bb72fb60fecb94a1
Vivien <samanthaservo09@gmail.com>
2024-05-06 22:05:15 +0800

    added the code for data preprocessing

--- existing_svm_algorithm/existing_svm.py
+++ existing_svm_algorithm/existing_svm.py
@@ -29,6 +29,31 @@ np.random.seed(500)
 Corpus = pd.read_csv(r"data/corpus.csv",encoding='latin-1')
 
 #data preprocessing
+# Step - a : Remove blank rows if any.
+Corpus['text'].dropna(inplace=True)
+# Step - b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
+Corpus['text'] = [entry.lower() for entry in Corpus['text']]
+# Step - c : Tokenization : In this each entry in the corpus will be broken into set of words
+Corpus['text']= [word_tokenize(entry) for entry in Corpus['text']]
+# Step - d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
+# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+for index,entry in enumerate(Corpus['text']):
+    # Declaring Empty List to store the words that follow the rules for this step
+    Final_words = []
+    # Initializing WordNetLemmatizer()
+    word_Lemmatized = WordNetLemmatizer()
+    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+    for word, tag in pos_tag(entry):
+        # Below condition is to check for Stop words and consider only alphabets
+        if word not in stopwords.words('english') and word.isalpha():
+            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
+            Final_words.append(word_Final)
+    # The final processed set of words for each iteration will be stored in 'text_final'
+    Corpus.loc[index,'text_final'] = str(Final_words)
 
 #prepare train and test datasets
 
